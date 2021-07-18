9d51611da0e3571f0e1b564b82893091283a8c52
Lucas Hideki Sekiya N <hidekilu@vivaldi.net>
2021-07-17 21:11:44 -0300

    removed unecessary code by clippy.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 mod my_image;
 
-use my_image::my_image::MyRgbImage;
+use my_image::MyRgbImage;
 use std::time::Instant;
 
 pub fn main() {
     let neko_image = image::open("./neko.jpg")
         .expect("Erro ao abrir imagem")
         .to_rgb8();
-    dbg!(neko_image.get(1));
     let tempo = Instant::now();
     let mut my_image = MyRgbImage::new(neko_image);
     my_image.mess_everything();
