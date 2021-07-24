8c73551a27173a0e262f6625fb999d26bedf5c9d
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-07-24 19:04:06 -0300

    Add error handling

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+use image::error::ImageError;
+
+#[derive(Debug)]
+pub enum Error {
+    IndexOutOfBounds,
+    ImageError(ImageError)
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+impl From<ImageError> for Error {
+    fn from(err: ImageError) -> Self {
+        Self::ImageError(err)
+    }
+}
