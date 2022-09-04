4e887ed20c835655804ef21da2c9f6a19a9a3aa7
eribito <57811230+eribito@users.noreply.github.com>
2022-09-04 15:34:27 -0300

    Criação da pasta de exemplo e inclusão de um

---
+++ src/exemplos/exemplo1.rs
@@ -0,0 +1,16 @@
+extern crate exemplo;
+
+use filty::my_image::MyRgbImage;
+use filty::my_image::RgbFilter;
+
+pub fn exemplo1() -> filty::error::Result<()> {
+    let neko_image = image::open("./neko.jpg")
+        .expect("Erro ao abrir imagem")
+        .to_rgb8();
+
+    MyRgbImage::new(neko_image)
+        .blend(RgbFilter::Magenta)
+        .save_image("./new_neko.jpg")?;
+
+    Ok(())
+}
